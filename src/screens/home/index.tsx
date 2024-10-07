3d94ad33f14e9f0f5aeb7dfe522fe27c7b612b08
Ayush-Bhardwaj05 <136813045+Ayush-Bhardwaj05@users.noreply.github.com>
2024-10-07 21:54:25 +0530

    Updated backend link in the project

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -156,7 +156,7 @@ export default function Home() {
 
     if (canvas) {
       const response = await axios.post(
-        "https://ai-calculator-backend-4353nxh5p-ayush-bhardwaj05s-projects.vercel.app/calculate",
+        "https://ai-calculator-backend-4353nxh5p-ayush-bhardwaj05s-projects.vercel.app",
         {
           image: canvas.toDataURL("image/png"),
           dict_of_vars: dictOfVars,
